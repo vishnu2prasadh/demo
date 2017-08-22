73bc9e877d2c3226fcef59dc5ed087a33585b719
Softsquare <selvasundari@softsquare.biz>
2017-08-22 16:25:36 +0530

    test prototype3

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/',function(req, res){
 
     res.send('Hi ')
 });
+app.get('/hi',function(req, res){
+
+    res.send('How Are You')
+});
 app.listen(7000,function(){
 
     console.log('Listening at 7000');
