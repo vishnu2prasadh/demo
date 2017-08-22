5e553ba9339115f5c036379d638cedd57b82dd6c
Softsquare <Softsquare>
2017-08-22 15:51:18 +0530

    Prototype I

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+var app=express();
+
+
+app.get('/',function(req, res){
+
+    res.send('Hi ')
+});
+app.listen(7000,function(){
+
+    console.log('Listening at 7000');
+});
